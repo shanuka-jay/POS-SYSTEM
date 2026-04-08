56ddc028568f0d19f0837e912fe4bf3debce8a1d
Shanuka Jayakodi <131529119+shanuka-jay@users.noreply.github.com>
2026-04-08 18:04:41 +0530

    Implement settings retrieval and update functionality
    Add getSettings and updateSettings functions to manage application settings.

--- pos-system/server/controllers/settingsController.js
+++ pos-system/server/controllers/settingsController.js
@@ -1,1 +1,31 @@
+const Settings = require('../models/Settings');
+
+exports.getSettings = async (req, res) => {
+  try {
+    let settings = await Settings.findOne();
+    if (!settings) {
+      settings = await Settings.create({});
+    }
+    res.json(settings);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.updateSettings = async (req, res) => {
+  try {
+    let settings = await Settings.findOne();
+    if (!settings) {
+      settings = await Settings.create(req.body);
+    } else {
+      settings = await Settings.findByIdAndUpdate(settings._id, req.body, {
+        new: true,
+        runValidators: true,
+      });
+    }
+    res.json(settings);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
