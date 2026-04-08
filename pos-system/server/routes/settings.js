e5c8ac830a7953867d7cbeeda3b6d407de1ac5b1
Shanuka Jayakodi <131529119+shanuka-jay@users.noreply.github.com>
2026-04-08 18:06:16 +0530

    Add settings routes with authentication middleware

--- pos-system/server/routes/settings.js
+++ pos-system/server/routes/settings.js
@@ -1,1 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const { getSettings, updateSettings } = require('../controllers/settingsController');
+const { protect, authorize } = require('../middleware/auth');
+
+router.use(protect);
+
+router.get('/', getSettings);
+router.put('/', authorize('admin'), updateSettings);
+
+module.exports = router;
 
