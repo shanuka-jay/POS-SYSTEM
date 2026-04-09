ba0ce4866f708ad044e2fc8b868d8f2bb36e00aa
BoyagodaRGAD <166887983+BoyagodaRGAD@users.noreply.github.com>
2026-04-09 16:24:20 +0530

    Add files via upload
    Add sales routes

--- pos-system/server/routes/sales.js
+++ pos-system/server/routes/sales.js
@@ -1,1 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const { createSale, getSales, getSale, getRecentSales } = require('../controllers/salesController');
+const { protect } = require('../middleware/auth');
 
+router.use(protect);
+
+router.post('/', createSale);
+router.get('/', getSales);
+router.get('/recent', getRecentSales);
+router.get('/:id', getSale);
+
+module.exports = router;
