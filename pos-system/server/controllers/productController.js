e5ddf441662998e3391901abda0e89980a9614d7
savindu.dilaksha147@gmail.com <savindu.dilaksha147@gmail.com>
2026-04-10 10:00:51 +0530

    .

--- pos-system/server/controllers/productController.js
+++ pos-system/server/controllers/productController.js
@@ -23,27 +23,7 @@ exports.getProducts = async (req, res) => {
   }
 };
 
-exports.getProduct = async (req, res) => {
-  try {
-    const product = await Product.findById(req.params.id);
-    if (!product) return res.status(404).json({ message: 'Product not found' });
-    res.json(product);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
 
-exports.createProduct = async (req, res) => {
-  try {
-    const product = await Product.create(req.body);
-    res.status(201).json(product);
-  } catch (error) {
-    if (error.code === 11000) {
-      return res.status(400).json({ message: 'SKU already exists' });
-    }
-    res.status(500).json({ message: error.message });
-  }
-};
 
 
 
